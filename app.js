64f5e3e938f85e5f9c8af9ad28e8d9b4bbed4449
Clement Pigeon <clementpigeon@gmail.com>
2014-01-16 16:38:11 +0100

    changes to mondrian and warhol

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
+var fs = require('fs');
 var app = express();
 
 // all environments
@@ -19,6 +19,7 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.multipart());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -29,8 +30,29 @@ if ('development' == app.get('env')) {
 }
 
 
-app.get('/users', user.list);
+app.post('/upload_img', upload_img);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+function upload_img(req, res, next){
+	var img = req.files.photo; 
+	var photo_name = img.name;
+	var photo_path = __dirname + '/public/uploads/' + photo_name;
+
+	fs.rename(img.path, photo_path, function(err){
+		if (err) return next(err);
+		res.end(JSON.stringify({
+			photo_path: '/uploads/' + photo_name,
+			photo_name: photo_name
+		}))
+	})
+}
+
+
+
+
+
+
